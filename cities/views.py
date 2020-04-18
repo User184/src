f00a9e6cfaf37ab433d4d67c537107f7bbb7aff4
User184 <yurii1355513@gmail.com>
2020-04-18 17:14:45 +0400

    3.17 Отображение на осн классов.DetailView.

--- cities/views.py
+++ cities/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
 from .models import City 
+from django.views.generic.detail import DetailView
 
 def home(request):
     cities = City.objects.all()  
     return render(request, 'cities/home.html', {'objects_list': cities}) 
+
+class CityDetailView(DetailView):
+    queryset = City.objects.all()
+    context_object_name ='objects'
+    template_name = 'cities/detail.html'
