531ee69105057888049cb54ad089723d18dbbf64
User184 <yurii1355513@gmail.com>
2020-04-24 20:22:47 +0400

    3.23.Messages

--- cities/views.py
+++ cities/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.views.generic.detail import DetailView
 from django.views.generic.edit import (CreateView, UpdateView, DeleteView)
+from django.contrib.messages.views import SuccessMessageMixin
+from django.contrib import messages
 from django.core.paginator import Paginator
 from django.urls import reverse_lazy
 
@@ -27,24 +29,27 @@ class CityDetailView(DetailView):
     context_object_name ='objects'
     template_name = 'cities/detail.html'
 
-class CityCreateView(CreateView):
+class CityCreateView(SuccessMessageMixin, CreateView):
     model = City
     form_class = CityForm
     template_name = 'cities/create.html'
     success_url = reverse_lazy('city:home')
+    success_message = 'Город успешно создан.'
 
-class CityUpdateView(UpdateView):
+class CityUpdateView(SuccessMessageMixin, UpdateView):
     model = City
     form_class = CityForm
     template_name = 'cities/Update.html'
     success_url = reverse_lazy('city:home')
+    success_message = 'Город отредактирован.'
 
 class CityDeleteView(DeleteView):
     model = City
-    template_name = 'cities/Delete.html'
+    # template_name = 'cities/Delete.html'
     success_url = reverse_lazy('city:home')
 
     # для моментального удаления объекта, без запроса подтверждения на 
     # отдельной форме html
     def get(self,request,*args, **kwargs):
+        messages.success(request, 'Город удален')
         return self.post(request, *args, **kwargs)
