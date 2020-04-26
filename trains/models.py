aa59e71d7c9914f940bc1cd1140f699191ef9a1f
User184 <yurii1355513@gmail.com>
2020-04-27 02:12:03 +0400

    4.25 Method clean

--- trains/models.py
+++ trains/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core.exceptions import ValidationError
 from cities.models import City
 
 class Train(models.Model):
@@ -16,3 +17,13 @@ class Train(models.Model):
 
     def __str__(self):
         return f'Поезд №{self.name} из {self.from_city} в {self.to_city}'
+
+    def clean(self, *args, **kwargs):
+        if self.from_city == self.to_city:
+            raise ValidationError('Измените город прибытия.')
+        qs = Train.objects.filter(from_city=self.from_city, to_city=self.to_city,
+                                    travel_time=self.travel_time).exclude(pk=self.pk)
+        if qs.exists():
+            raise ValidationError('Измените время в пути.')
+
+        return super(Train, self).clean(*args, **kwargs)
