60c7ee1e48ad372837113d0696bddd25626af57b
User184 <yurii1355513@gmail.com>
2020-04-29 14:52:25 +0400

    4.27. Форма для добавления новой записи поезда

--- trains/urls.py
+++ trains/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-from .views import (home, )
+from .views import (home, TrainCreateView)
 
 urlpatterns = [
     # path('detail/<int:pk>/', CityDetailView.as_view(), name='detail'),
     # path('update/<int:pk>/', CityUpdateView.as_view(), name='update'),
     # path('delete/<int:pk>/', CityDeleteView.as_view(), name='delete'),
-    # path('add/', CityCreateView.as_view(), name='add'),
+    path('add/', TrainCreateView.as_view(), name='add'),
     path('', home, name='home'),
 ]
